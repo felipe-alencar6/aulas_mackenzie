5ad25d1f7d0952eead1879dfac980530faf154e5
Felipe Alencar Giacomini <89051950+felipe-alencar6@users.noreply.github.com>
2021-12-08 19:34:38 -0300

    Basically a times table
    Exercise made during the first semester test in programming logic;
    Ity will show the times table for the first number until the second number.

---
+++ provap2.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = int(input())
+z = int(input())
+k = int
+while(z<y):
+    x = int(input())
+    y = int(input())
+    z = int(input())
+if(z>=y):
+    while(y<=z):
+        k= x*y
+        y+=1
+        print(k)
